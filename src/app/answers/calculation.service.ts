e13bdc1317c418f0f3efb6ed57a02be8581dd657
Eoghan O'Neill <eoneill@palantirsolutions.com>
2018-03-28 14:50:42 +0100

    q5

--- src/app/answers/calculation.service.ts
+++ src/app/answers/calculation.service.ts
@@ -12,6 +12,8 @@ export class CalculationService {
   private solutions = [this.q1SumMultiples,
           this.q2EvenFibonacci,
           this.q3LargestPrimeFactor,
+          this.q4LargestPalindrome,
+          this.q5SmallestMultiple
         ];
 
   q1SumMultiples(multiples: number[] = [3, 5], below: number = 1000): number {
@@ -40,7 +42,7 @@ export class CalculationService {
     return total;
   }
 
-  q3LargestPrimeFactor(n: number = 600851475143): number { // 600851475143
+  q3LargestPrimeFactor(n: number = 600851475143): number {
     const factors = [];
     let f = 2; // first possible prime factor
     while (n > 1) {
@@ -56,28 +58,61 @@ export class CalculationService {
   }
 
   q4LargestPalindrome(): number {
-    let pal = 0;
-    let startFrom = 999 ** 2;
-    while (startFrom > 0) {
-      if (isPalindrome(startFrom.toString())) {
-        //
-      } else {
-        startFrom --;
-      }
-    }
-    return 0;
-
-    function isPalindrome(s: string): boolean {
-
-      while (s.length > 0) {
-        if (s[0] === s[s.length - 1]) {
-          s = s.substr(1, s.length - 2);
-        } else {
-          return false;
+    let product = 999 ** 2;
+    while (product > 0) {
+      if (this.isPalindrome(product.toString())) {
+        // check if palindrom is divisible by two 3-digit numbers
+        let divisor = 999, keepGoing = true;
+        while (keepGoing) {
+          if (Math.floor(product / divisor) > 999) {
+            // gone too far - if the second factor is > 999 then it's not a solution
+            keepGoing = false;
+          } else if (product % divisor === 0) {
+            console.log(`Palindromic number ${product} is a product of ${divisor} and ${product / divisor}`);
+            return product; // bingo
+          } else {
+            divisor --;
+          }
         }
       }
-      return true;
+      product --;
     }
+    return 0; // failed
+  }
+
+  private isPalindrome(s: string): boolean {
+
+    while (s.length > 0) {
+      if (s[0] === s[s.length - 1]) {
+        s = s.substr(1, s.length - 2);
+      } else {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  q5SmallestMultiple(max: number = 20): number {
+    // return smallest common multiple of all numbers up to <max>
+    let foundIt = false, multiple = max - 1;
+    while (!foundIt) {
+      multiple ++;
+      let factor = max;
+      while (factor > 1) {
+        // crude - still checking the divisibility of numbers that are factors of previous divisors,
+        // e.g. if a number is divisible by 16, we shouldn't need to check if it is divisible by 8, 4 or 2
+        if (multiple % factor !== 0) {
+          factor = -1; // not divisible - break out of inner while and continue to check next number
+        } else if (factor === 2) {
+          // have successfully divided evenly by all numbers down to 2
+          factor = -1; // break out of inner while
+          foundIt = true; // break out of outer while
+        } else {
+          factor--;
+        }
+      }
+    }
+    return multiple;
   }
 
   async executeQuestions(): Promise<IAnswer[]> {
